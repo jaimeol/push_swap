d569a06dc0450a5c6753ae28019e716232e05242
Jaime Olivares Cepeda <jolivare@c2r2s6.42madrid.com>
2024-01-22 15:33:54 +0100

    El de 3 va nitido

--- algorithm.c
+++ algorithm.c
@@ -17,15 +17,25 @@ static void	sort3(t_list **stack)
 	t_list	*last;
 
 	last = ft_lstlast(*stack);
-	if ((*stack)-> num < last-> num && (*stack)-> num < (*stack)-> next-> num)
+	if ((*stack)->num < last->num && (*stack)->num < (*stack)->next->num)
 	{
-		if (last-> num > (*stack)-> num)
+		if (last->num < (*stack)->next->num)
 			ra(stack);
-	}
-	else if ((*stack)-> num > last-> num)
-	{
-		if ((*stack)-> num < (*stack)-> next-> num)
+		else if (last->num > (*stack)->next->num)
+		{
+			ra(stack);
 			sa(stack);
+		}
+	}
+	else if ((*stack)->num > last->num)
+	{
+		if ((*stack)->num < (*stack)->next->num)
+			sa(stack);
+		else if ((*stack)->next->num)
+		{
+			rra(stack);
+			sa(stack);
+		}
 	}
 }
 
@@ -34,7 +44,7 @@ static void	sort5(t_list **stack_a, t_list **stack_b)
 	t_list	*last;
 
 	last = ft_lstlast(*stack_a);
-	if ((*stack_a)-> next-> num < (*stack_a)-> num)
+	if ((*stack_a)->next->num < (*stack_a)->num)
 	{
 		printf("Hola\n");
 		pb(stack_a, stack_b);
@@ -50,7 +60,7 @@ int	main()
 	t_list	*node1, *node2, *node3, *node4, *node5;
 
 	node1 = malloc(sizeof(t_list));
-	node1 -> num = 4;
+	node1 -> num = 5;
 	node1 -> next = NULL;
 
 	node2 = malloc(sizeof(t_list));
@@ -58,7 +68,7 @@ int	main()
 	node2 -> next = NULL;
 
 	node3 = malloc(sizeof(t_list));
-	node3 -> num = 5;
+	node3 -> num = 4;
 	node3 -> next = NULL;
 
 	node4 = malloc(sizeof(t_list));
@@ -89,7 +99,7 @@ int	main()
 		printf("%d\n", aux -> num);
 		aux = aux -> next;
 	}
-	sort5(&stack_a, &stack_b);
+	sort3(&stack_a);
 	printf("Stack A después de sort5:\n");
 	aux = stack_a;
 	while (aux != NULL)
